601161ee62cc5d3bf7b14039fe5d7c3f63511ec3
kauapaixao <137851402+kauapaixao@users.noreply.github.com>
2024-11-19 20:07:54 +0000

    commit

---
+++ javaproject/src/main/java/com/example/javaproject/Carro.java
@@ -0,0 +1,14 @@
+import lombok.Setters;
+import lombok.Getters;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getters
+@Setters
+@NoArgsConstructor
+
+public class Carro {
+    private String modelo;
+    private String marca;
+}
